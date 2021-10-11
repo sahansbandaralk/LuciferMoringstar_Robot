2c267f9a9b277f34aaa0ca5cff62ad0375e36cd8
sahansbandaralk <87052430+sahansbandaralk@users.noreply.github.com>
2021-10-11 22:59:45 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -13,21 +13,21 @@ from .Channel import (
 HELP = """
 <b><u>Basic Commads</u></b>
 **
-/start : check if am alive or dead
-/about : About Me
+   /start : check if am alive or dead
+   /about : About Me
 **
 <b><u>Bot Owner Only</u></b>
 **
-  /broadcast Replay Any Message or Media
-  /stats user Status
-  /ban_user  click ban_user more info
-  /unban_user click unban_user more info
-  /banned_users Banned User Details
-  /total How Many Files Added In Database
-  /logger  Get Logs
-  /delete Delete File From Database **
+  /broadcast : Replay Any Message or Media
+  /stats user : Status
+  /ban_user : click ban_user more info
+  /unban_user : click unban_user more info
+  /banned_users : Banned User Details
+  /total : How Many Files Added In Database
+  /logger : Get Logs
+  /delete : Delete File From Database **
 """
 
 ABOUT = """
- <b>○ Creator : @Anonymous_Sender_SEC </b>\n○ Language : <code>Python3</code>\n○ Channel : @SECLK\n○ Main Channel : @NetflixMovieslk</b>",
+ <b>○Creator : @Anonymous_Sender_SEC </b>\n<b>○Language : Python3</b>\n<b>○Channel : @SECLK</b>\n<b>○Main Channel : @NetflixMovieslk</b>",
 """
