472aa84634993e7005e757ea1dbbc33f41da603b
sahansbandaralk <87052430+sahansbandaralk@users.noreply.github.com>
2021-10-11 22:28:17 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -331,7 +331,8 @@ async def bot_info(bot, message):
     buttons = [
         [
             
-            InlineKeyboardButton('○ More Bots', url='https://t.me/SECLK')
+             InlineKeyboardButton('○ More Bots', url='https://t.me/SECLK'),
+              InlineKeyboardButton('○ Main Channel', url='https://t.me/NetflixMovieslk')
         ]
         ]
     await message.reply(text=f"{ABOUT}", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
