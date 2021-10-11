a9d189ea408dcb65eefb1c03c5ff61a9815b49fc
sahansbandaralk <87052430+sahansbandaralk@users.noreply.github.com>
2021-10-11 22:31:15 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -11,13 +11,13 @@ from .Channel import (
 )
 
 HELP = """
-<b>Basic Commads</b>
-
+<b><u>Basic Commads</u></b>
+**
 /start : check if am alive or dead
 /about : About Me
-
-<b>Bot Owner Only</b>
-
+**
+<b><u>Bot Owner Only</u></b>
+**
   /broadcast Replay Any Message or Media
   /stats user Status
   /ban_user  click ban_user more info
@@ -25,9 +25,9 @@ HELP = """
   /banned_users Banned User Details
   /total How Many Files Added In Database
   /logger  Get Logs
-  /delete Delete File From Database
+  /delete Delete File From Database **
 """
 
 ABOUT = """
- text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Channel : @SECLK\n○ Main Channel : @NetflixMovieslk</b>",
+ <b>○ Creator : @Anonymous_Sender_SEC </b>\n○ Language : <code>Python3</code>\n○ Channel : @SECLK\n○ Main Channel : @NetflixMovieslk</b>",
 """
