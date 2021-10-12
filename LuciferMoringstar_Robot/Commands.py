e497fa3729e661202f89f80afbfa97661f1c2246
sahansbandaralk <87052430+sahansbandaralk@users.noreply.github.com>
2021-10-12 12:30:17 +0530

    Update Commands.py

--- LuciferMoringstar_Robot/Commands.py
+++ LuciferMoringstar_Robot/Commands.py
@@ -84,7 +84,7 @@ async def start(bot, message):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🎖 DEPLOY YOURS 🎖', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('ᴍᴏʀᴇ ʙᴏᴛꜱ 🤖', url=f'{TUTORIAL}')
                     ]
                     ]
                 await bot.send_cached_media(
